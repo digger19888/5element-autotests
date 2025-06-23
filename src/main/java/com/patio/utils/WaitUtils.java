81d677be231839f5d53729f7c81d36cc62f743c0
dkoreshkov <d.koreshkov88@gmail.com>
2025-06-23 16:53:40 +0300

    add base test, base page, wait utils

--- src/main/java/com/patio/utils/WaitUtils.java
+++ src/main/java/com/patio/utils/WaitUtils.java
@@ -1,4 +1,25 @@
 package com.patio.utils;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class WaitUtils {
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    public WaitUtils(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+    }
+
+    public WebElement forElementVisible(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    // Другие методы ожидания...
 }
--- src/test/java/com/patio/BaseTest.java
+++ src/test/java/com/patio/BaseTest.java
@@ -1,4 +1,59 @@
 package com.patio;
 
+import com.patio.config.BrowserConfig;
+import com.patio.config.DeviceConfig;
+import com.patio.config.EnvironmentConfig;
+import com.patio.config.TestConfig;
+import com.patio.drivers.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+
+import java.lang.reflect.Method;
+
+@Listeners({TestListener.class})
 public class BaseTest {
+    @BeforeMethod
+    @Parameters({"environment", "browser", "device", "isMobile", "isTablet"})
+    public void setUp(
+            @Optional("test") String environment,
+            @Optional("chrome") String browser,
+            @Optional String device,
+            @Optional("false") boolean isMobile,
+            @Optional("false") boolean isTablet,
+            Method method) {
+
+        // Загрузка конфигураций
+        EnvironmentConfig envConfig = JsonUtils.loadEnvironmentConfig(environment);
+        BrowserConfig browserConfig = JsonUtils.loadBrowserConfig(browser);
+        DeviceConfig deviceConfig = isMobile || isTablet ? JsonUtils.loadDeviceConfig(device) : null;
+
+        // Создание тестовой конфигурации
+        TestConfig config = TestConfig.builder()
+                .environment(envConfig)
+                .browser(browserConfig)
+                .device(deviceConfig)
+                .isMobile(isMobile)
+                .isTablet(isTablet)
+                .testName(method.getName())
+                .build();
+
+        // Инициализация драйвера
+        WebDriverManager.initDriver(config);
+
+        // Открытие базового URL
+        WebDriverManager.getDriver().get(envConfig.getBaseUrl());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        WebDriverManager.quitDriver();
+    }
+
+    protected WebDriver getDriver() {
+        return WebDriverManager.getDriver();
+    }
+
+    protected TestConfig getConfig() {
+        return WebDriverManager.getConfig();
+    }
 }
