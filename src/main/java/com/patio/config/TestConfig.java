35ef024be5967a25fc0bdfc776b584aa245beed9
dkoreshkov <d.koreshkov88@gmail.com>
2025-06-23 16:22:31 +0300

    add configs

--- src/main/java/com/patio/config/TestConfig.java
+++ src/main/java/com/patio/config/TestConfig.java
@@ -1,4 +1,19 @@
 package com.patio.config;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TestConfig {
+    private BrowserConfig browser;
+    private DeviceConfig device;
+    private EnvironmentConfig environment;
+    private boolean isMobile;
+    private boolean isTablet;
+    private String testName;
 }
